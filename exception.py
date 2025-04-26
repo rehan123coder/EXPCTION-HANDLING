7568e2450a881aa03ff93c7bbe407b981839d5c2
rehan123coder <rehankhann@gmail.com>
2025-04-26 10:53:04 +0500

    age

--- exception.py
+++ exception.py
@@ -1,6 +1,6 @@
 try:
-    num1,num2=eval(input("enter two numbers comma sperated by comma :"))
-    result = num1/num2
+    age=int(input("enter your age :"))
+    result = age=18
     print("resulst is",result)
 except ZeroDivisionError:
     print("you hve a zerodivisionerror")
